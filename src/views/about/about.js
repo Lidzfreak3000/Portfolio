201928e2b7cc44a4e3d3efcd7c4ef764aa0a3786
John Duncan <jduncan@equitytech.consulting>
2025-04-03 21:35:59 -0400

    Added package lock to gitignore and added an idea for the About page

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -2,9 +2,19 @@ import Section from "../../components/section/Section";
 
 function About(props) {
     return (
-    <Section id="about" setVisibleSection={props.setVisibleSection}>
-        <h1 className=''> Welcome to my personal site 😎</h1>
-    </Section>
+        <Section id="about" setVisibleSection={props.setVisibleSection}>
+            <figure className="diff h-full w-full" tabIndex={0}>
+                <div className="diff-item-1" role="img">
+                    <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+                        DAISY
+                    </div>
+                </div>
+                <div className="diff-item-2" role="img" tabIndex={0}>
+                    <div className="bg-base-200 grid place-content-center text-9xl font-black">DAISY</div>
+                </div>
+                <div className="diff-resizer"></div>
+            </figure>
+        </Section>
     );
 }
 
