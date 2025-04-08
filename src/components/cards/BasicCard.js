a48997beff5e0c9f5a9d4c9e32b35da90b308d24
John Duncan <jduncan@equitytech.consulting>
2025-04-08 13:14:48 -0400

    Basic card layout is resotred

--- src/components/cards/BasicCard.js
+++ src/components/cards/BasicCard.js
@@ -3,8 +3,8 @@ import './BasicCard.css';
 function BasicCard(props) {
 
     return (
-        <div className="card-wrapper bg-neutral rounded-box">
-            <div className="card-content">
+        <div className="card-wrapper rounded-box rounded-2xl overflow-hidden">
+            <div className="card-content rounded-box rounded-2xl p-4">
                 {props.children}
             </div>
         </div>
